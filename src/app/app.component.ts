435f359a43926e348c9b05395a4627c2457e8d08
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-09 10:04:27 -0700

    Adding sync block function in component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  // number that starts at 0
+  blockNumber: number | string | undefined;
+  provider: ethers.providers.BaseProvider;
+  // constructor function
+  constructor() {
+    this.blockNumber = "Loading...";
+    this.provider = ethers.getDefaultProvider('goerli');
+    // here we are listenting to the promise and when it is completed then receiving the block object
+    this.provider.getBlock('latest').then((block) => {
+      this.blockNumber = block.number;
+    });
+  }
+
+  syncBlock() {
+
+  }
 }
